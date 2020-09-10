94af729ea3f268aaafbff7bc13b38becc8363812
Emma-Solano <solano.c.emmanuel@gmail.com>
2020-09-09 22:36:49 -0500

    Progress until step 85

--- src/Util/Spotify.js
+++ src/Util/Spotify.js
@@ -1,3 +1,32 @@
-const Spotify = {}
+const appClientId = 'ed51599a8a094243aa0be1af4a6a170f'
+const redirectURI = "http://localhost:3000/"
+
+let userAccessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if(userAccessToken) {
+            return userAccessToken
+        }
+        //Looking for access tokens and expires date
+        let accessTokenMatch = window.location.href.match(/access_token=([^&]*)/)
+        let expireDateMatch = window.location.href.match(/expires_in=([^&]*)/)
+
+        if(accessTokenMatch && expireDateMatch) {
+            userAccessToken = accessTokenMatch
+            let expiresIn = Number(expireDateMatch[1])
+            //Clearing new parameter in URL when token expires 
+            window.setTimeout(() => userAccessToken = '', expiresIn * 1000)
+            window.history.pushState('Access Token', null, '/')
+            return userAccessToken
+        } else {
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${appClientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`
+            window.location = accessUrl;
+        }
+    },
+    search(term) {
+
+    }
+}
 
 export default Spotify
